01e46d7975640cf8ad5f88b9efdae53464ffd322
Nhan Phan <hieunhan2000@gmail.com>
2023-05-10 14:36:26 +0700

    fnt.Println db

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	mysql "mysql-replica/adapter"
 )
 
 func main() {
-	mysql.NewDatabase()
+	db := mysql.NewDatabase()
+
+	fmt.Println(db.ReadDB)
+	fmt.Println(db.WriteDB)
 }
